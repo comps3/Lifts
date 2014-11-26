27bc8113ecfbcc8b93d8a41b3c8acad4af6ee5c8
bhuynh@csumb.edu <brianhuynh@Brians-MacBook-Air.local>
2014-11-26 14:57:18 -0800

    Cleaned up experiments.

--- Lifts/ViewController.swift
+++ Lifts/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITextFieldDelegate
     @IBOutlet weak var segmentedControl: UISegmentedControl!
     //@IBOutlet weak var weightSlider: UISlider!
 
-    let weightList: [Double] = [2.5, 5, 10, 25, 35, 45]
+    let weightList: [Double] = [1.25 ,2.5, 5, 10, 25, 35, 45]
     var barbellWeights = NSMutableArray()
     var plateTally = NSMutableArray()
     var poundsOrKilo: Bool = true
@@ -31,50 +31,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITextFieldDelegate
         navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
         totalWeight.textColor = UIColor.redColor()
         
-        // Declaration for adding a done button to keyboard
-        var numberPadDoneButton = NumberPadDoneBtn(frame: CGRectMake(0, 0, 1, 1))
-        self.totalWeight.inputAccessoryView = numberPadDoneButton
-        
         totalWeight.delegate = self
-        
-        /*
-        if exerciseSwitch == 0 {
-            if dataSafe.doubleForKey("benchTotal") != 0.000000
-            {
-                totalWeight.text = String(format:"%f", dataSafe.doubleForKey("benchTotal"))
-            }
-        }
-            
-        else if exerciseSwitch == 1 {
-            
-            if dataSafe.doubleForKey("deadTotal") != 0.000000
-            {
-                totalWeight.text = String(format:"%f", dataSafe.doubleForKey("deadTotal"))
-            }
-        }
-        else if exerciseSwitch == 2 {
-            
-            if dataSafe.doubleForKey("swatTotal") != 0.000000
-            {
-                totalWeight.text = String(format:"%f", dataSafe.doubleForKey("swatTotal"))
-            }
-        }
-        else if exerciseSwitch == 3 {
-         
-            
-            if dataSafe.doubleForKey("overheadTotal") != 0.000000
-            {
-                totalWeight.text = String(format:"%f", dataSafe.doubleForKey("overheadTotal"))
-            }
-        }
-        else if exerciseSwitch == 4 {
-            
-            if dataSafe.doubleForKey("rowsTotal") != 0.000000
-            {
-                totalWeight.text = String(format:"%f", dataSafe.doubleForKey("rowsTotal"))
-            }
-        }
-       */
         
         tableView.rowHeight = 40
         // Removes table view lines
@@ -94,40 +51,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITextFieldDelegate
         // Dispose of any resources that can be recreated.
     }
     
-    /*
-    Future: Save a person's weight lifting data
-    
-    @IBAction func indexChange(sender: AnyObject)
-    {
-        switch segmentedControl.selectedSegmentIndex
-        {
-            case 0:
-                    exerciseSwitch = 0
-                    tableView.reloadData()
-            
-            case 1:
-                    exerciseSwitch = 1
-                    tableView.reloadData()
-            
-            case 2:
-                    exerciseSwitch = 2
-                    tableView.reloadData();
-            
-            case 3:
-                    exerciseSwitch = 3
-                    tableView.reloadData();
-            
-            case 4:
-                    exerciseSwitch = 4
-                    tableView.reloadData();
-            
-            default: print("What backup plans!")
-            
-        }
-        
-    }
-    */
-
+ 
     func numberOfSections(tableView: UITableView!) -> Int
     {
         return 1
@@ -144,57 +68,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITextFieldDelegate
     {
         var cell = tableView.dequeueReusableCellWithIdentifier("barbellPlate") as BarbellPlate
         
-        /*
-        var tempArray: NSArray = []
-        var temp2Array:NSArray = []
-        
-      
-        if exerciseSwitch == 0 {
-            if dataSafe.arrayForKey("benchTally") != nil && dataSafe.arrayForKey("benchWeights") != nil
-            {
-                tempArray = dataSafe.arrayForKey("benchTally")!
-                temp2Array = dataSafe.arrayForKey("benchWeights")!
-            }
-        }
-            
-        else if exerciseSwitch == 1 {
-            
-            if dataSafe.arrayForKey("deadTally") != nil && dataSafe.arrayForKey("deadWeights") != nil
-            {
-                tempArray = dataSafe.arrayForKey("deadTally")!
-                temp2Array = dataSafe.arrayForKey("deadWeights")!
-                
-            }
-            
-        }
-        else if exerciseSwitch == 2 {
-            
-            if dataSafe.arrayForKey("swatTally") != nil && dataSafe.arrayForKey("swatWeights") != nil
-            {
-                tempArray = dataSafe.arrayForKey("swatTally")!
-                temp2Array = dataSafe.arrayForKey("swatWeights")!
-                
-            }
-        }
-        else if exerciseSwitch == 3 {
-            
-            if dataSafe.arrayForKey("overheadTally") != nil && dataSafe.arrayForKey("overheadWeights") != nil
-            {
-                tempArray = dataSafe.arrayForKey("overheadTally")!
-                temp2Array = dataSafe.arrayForKey("overheadWeights")!
-                
-            }
-        }
-        else if exerciseSwitch == 4 {
-            
-            if dataSafe.arrayForKey("rowsTally") != nil && dataSafe.arrayForKey("rowsWeights") != nil
-            {
-                tempArray = dataSafe.arrayForKey("rowsTally")!
-                temp2Array = dataSafe.arrayForKey("rowsWeights")!
-            }
-        }
-        */
-        
         cell.plateCategory.text = "\(barbellWeights[indexPath.row])"
         cell.plateQty.text = "\(plateTally[indexPath.row])"
         
@@ -209,9 +82,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITextFieldDelegate
     {
         let barbell = 45.0
         var barbellPlateSide: Double = 0.0
-        
-        // Can't find keyplane that supports type 4 for keyboard iPhone-Portrait-NumberPad; using 3876877096_Portrait_iPhone-Simple-Pad_Default
-    
 
         var total_lift = Double((totalWeight.text as NSString).doubleValue)
         var running_total:Double = 0.0
@@ -242,34 +112,10 @@ class ViewController: UIViewController, UITableViewDelegate, UITextFieldDelegate
         
         running_total = total_lift - barbell
         running_total /= 2
-        
-        /*
-        if exerciseSwitch == 0 {
-            dataSafe.setDouble(total_lift, forKey: "benchTotal")
-            dataSafe.synchronize()
-        }
-        
-        else if exerciseSwitch == 1 {
-            dataSafe.setDouble(total_lift, forKey: "deadTotal")
-            dataSafe.synchronize()
-        }
-        else if exerciseSwitch == 2 {
-            dataSafe.setDouble(total_lift, forKey: "swatTotal")
-            dataSafe.synchronize()
-        }
-        else if exerciseSwitch == 3 {
-            dataSafe.setDouble(total_lift, forKey: "overheadTotal")
-            dataSafe.synchronize()
-        }
-        else if exerciseSwitch == 4 {
-            dataSafe.setDouble(total_lift, forKey: "rowsTotal")
-            dataSafe.synchronize()
-        }
-        */
-        
+              
         while running_total != 0
         {
-            for index in 0...5
+            for index in 0...weightList.count-1
             {
                 
                 // Checks if user's lift goal less than a larger plate
@@ -332,7 +178,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITextFieldDelegate
                 }
                  
                 // Running total is greater than 45 pounds
-                else if running_total >= weightList[index] && index == 5
+                else if running_total >= weightList[index] && index == weightList.count-1
                 {
                     running_total -= weightList[index]
                     
@@ -384,36 +230,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITextFieldDelegate
         }
         
         reverseArrays()
-        
-        /*
-        if exerciseSwitch == 0 {
-            dataSafe.setObject(plateTally, forKey: "benchTally")
-            dataSafe.setObject(weightList, forKey: "benchWeights")
-            dataSafe.synchronize()
-        }
-            
-        else if exerciseSwitch == 1 {
-            dataSafe.setObject(plateTally, forKey: "deadTally")
-            dataSafe.setObject(weightList, forKey: "deadWeights")
-            dataSafe.synchronize()
-        }
-        else if exerciseSwitch == 2 {
-            dataSafe.setObject(plateTally, forKey: "swatTally")
-            dataSafe.setObject(weightList, forKey: "swatWeights")
-            dataSafe.synchronize()
-        }
-        else if exerciseSwitch == 3 {
-            dataSafe.setObject(plateTally, forKey: "overheadTally")
-            dataSafe.setObject(weightList, forKey: "overheadWeights")
-            dataSafe.synchronize()
-        }
-        else if exerciseSwitch == 4 {
-            dataSafe.setObject(plateTally, forKey: "rowsTally")
-            dataSafe.setObject(weightList, forKey: "rowsWeights")
-            dataSafe.synchronize()
-        }
-        */
-        
         tableView.reloadData()
     }
     
